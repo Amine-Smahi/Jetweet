64a8c81188e232521c029605e0a6090fc17c92cc
Amine-Smahi <Mohammed.Amine.Smahi@gmail.com>
2018-08-28 22:32:45 +0100

    Add starting project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jetweet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
